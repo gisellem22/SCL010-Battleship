aa7042cf5571e9625b866a49026b729f11904d56
Giselle Machado <gisellemachadoyes@gmail.com>
2019-12-01 14:30:13 -0300

    se agrega metodo sendUser2 y boton join

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 
-import { UserService } from './../../services/user.service';
-
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-home',
@@ -11,55 +11,64 @@ import { UserService } from './../../services/user.service';
 })
 export class HomeComponent implements OnInit {
 
-  users: any;
-  user:any = {};
-  
+  user: User;
+  // user:any = {
+  //   user1: null,
+  //   user2: null
+  // }
+
   constructor(public afAuth: AngularFireAuth, public userService: UserService) {
     // this.users = userService.user;
     // console.log(this.users)
-   }
+  }
 
   ngOnInit() {
     this.onAuthStateChange();
     this.userService.getUser().subscribe(user => {
-      console.log(user)
+      console.log("Base de Datos - ROOM", user)
     })
   }
 
-  login() {
+  login(choise) {
     console.log("hola")
     this.afAuth.auth.signInAnonymously()
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log("ERROR",errorCode, errorMessage)
-      // ...
-    });
-    this.sendUser(this.user)
+      .then(anonymous => {
+        console.log("Anónimo", anonymous.user.uid)
+        this.user = JSON.parse(JSON.stringify(new User(anonymous.user.uid, null)));
+        if (choise === "start") {
+          // this.user.user1= anonymous.user.uid;
+          console.log(this.user)
+          this.sendUser1(this.user)
+        } else if (choise === "join") {
+          this.sendUser2(anonymous.user.uid)
+        }
+
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("ERROR", errorCode, errorMessage)
+        // ...
+      });
   };
-  
+
   onAuthStateChange() {
     this.afAuth.auth.onAuthStateChanged(user => {
-      console.log(user)
-      console.log("ID USUARIO",user.uid)
-      this.user.userId = user.uid;
-      console.log(this.user)
-      // if (user) {
-        // User is signed in.
-        // var isAnonymous = user.isAnonymous;
-        // var uid = user.uid;
-        // ...
-        // console.log(isAnonymous,uid)
-      // } else {
-        // User is signed out.
-        // ...
-      // }
-      // ...
-    })
+      if (user) {
+        console.log("USER is signed IN", user)
+      } else {
+        console.log("User is signed out")
+      }
+    });
   };
-sendUser(user) {
-this.userService.addUser(user);
+
+  sendUser1(user: User) {
+    this.userService.addUser1(user);
+  }
+
+  sendUser2(user: string) {
+    this.userService.addUser2(user)
   }
 
 }
