513d62c414bb957722438dbd448e2c4c0dd456ba
FabyTapia <ftapiamorales@gmail.com>
2019-12-03 15:43:59 -0300

    actualizacion de prueba unitaria

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -19,7 +19,7 @@ describe('UserService', () => {
     
   }));
 
- it('should be created', () => {
+it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
     expect(service).toBeTruthy();
   });
