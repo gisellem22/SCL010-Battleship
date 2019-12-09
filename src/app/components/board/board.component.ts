370f700e756f93c5ae6e9a114a168be7e589b456
Giselle Machado <gisellemachadoyes@gmail.com>
2019-12-08 21:28:44 -0300

    se agrega funcionalidad de elegir donde esconder el tesoro, la funcion hideTreasures()

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -7,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   squares: any[];
-  xIsNext: boolean;
+  index: boolean;
   winner: string;
+  // player1: string = 'p1';
+  // player2: string = 'p2';
+  // player: boolean;
+  arrOfIdx: number[] = []
+  areTreasuresHidden:any = {
+    player1: false
+  }
 
   constructor() { }
 
@@ -19,14 +26,34 @@ export class BoardComponent implements OnInit {
   newGame() {
     this.squares = Array(9).fill(null);
     this.winner = null;
-    this.xIsNext = true;
+    this.index = true;
   };
 
-  get player() {
-    return this.xIsNext ? 'X':'O';
-  };
-
-  makeMove(idx: number) {
-    console.log(idx)
-  };
+  // play(value: boolean) {
+  //   console.log(value)
+  //   if (value) {
+  //     return this.index = true;
+  //   } else {
+  //     return this.index = false;
+  //   }
+  // };
+  hideTreasures(idx:number) {
+    if(this.arrOfIdx.length <3){
+      this.arrOfIdx.push(idx)
+      this.index = true;
+      this.squares.splice(idx, 1, this.index);
+      console.log(this.arrOfIdx)
+      this.changeHiddenState()
+    } else {
+      console.log("hasta aqui nomás")
+    }
+console.log(this.arrOfIdx)
+  }
+changeHiddenState() {
+if (this.arrOfIdx.length === 3) {
+  console.log("hey")
+  this.areTreasuresHidden.player1 = true;
+}
+console.log(this.areTreasuresHidden)
+}
 }
