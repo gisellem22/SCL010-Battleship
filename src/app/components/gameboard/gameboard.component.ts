e50ff6da8165a4584b9650495c27874098b320ee
Giselle Machado <gisellemachadoyes@gmail.com>
2019-12-10 23:53:38 -0300

    prueba con cambio de funciones de los componentes a board service

--- src/app/components/gameboard/gameboard.component.ts
+++ src/app/components/gameboard/gameboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardsService } from '../../services/boards.service';
+
 
 @Component({
   selector: 'app-gameboard',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gameboard.component.css']
 })
 export class GameboardComponent implements OnInit {
-
-  constructor() { }
+ 
+  constructor(public boardsService: BoardsService) { }
 
   ngOnInit() {
   }
 
+  newGame() {
+    console.log("uii")
+    this.boardsService.getAnswer({
+      squares: Array(9).fill(null),
+      winner: null,
+      index: true,
+      arrOfIdx: [],
+      hidddingAt:[]
+      })
+  }
 }
