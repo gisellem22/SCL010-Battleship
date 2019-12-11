b8b867c35fcc053fb3ff83326dd63740fdf3ef56
Giselle Machado <gisellemachadoyes@gmail.com>
2019-12-11 14:49:33 -0300

    agregando estilo al template del component enemy board

--- src/app/components/enemy-board/enemy-board.component.ts
+++ src/app/components/enemy-board/enemy-board.component.ts
@@ -8,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class EnemyBoardComponent implements OnInit {
 
   squares: any[];
-  xIsNext: boolean;
+  index: boolean;
   winner: string;
+  arrOfIdx: number[] = [];
+  hidddingAt: number[];
+  
+  hidding: any = {
+    player1: false
+  };
 
   constructor() { }
 
@@ -20,41 +26,80 @@ export class EnemyBoardComponent implements OnInit {
   newGame() {
     this.squares = Array(9).fill(null);
     this.winner = null;
-    this.xIsNext = true;
+    this.index = true;
+    this.arrOfIdx = [];
   };
 
-  get player() {
-    return this.xIsNext ? 'X':'O';
+  // play(value: boolean) {
+  //   console.log(value)
+  //   if (value) {
+  //     return this.index = true;
+  //   } else {
+  //     return this.index = false;
+  //   }
+  // };
+  playGame(idx:number) {
+    console.log(this.hidding.player1);
+    if (this.hidding.player1 ===false){
+      this.hideTreasures(idx)
+    } else {
+      this.makeMove(idx)
+    }
   };
+  
+  hideTreasures(idx: number) {
+    //si no existe nada en el arreglo, guarda el primer indice
+    if (this.arrOfIdx.length < 1) {
+      this.arrOfIdx.push(idx)
+      this.index = true;
+      this.squares.splice(idx, 1, this.index);
+      console.log(this.arrOfIdx)
+      //si el arreglo tiene de 1 a 3 indices
+    } else if (this.arrOfIdx.length > 0 && this.arrOfIdx.length < 3) {
+      // impide pushear el mismo indice
+      if (this.arrOfIdx.includes(idx)) {
+        console.log("try again")
+        //si el indice no está contenido en el arreglo, pushealo
+      } else {
+        this.arrOfIdx.push(idx)
+        this.index = true;
+        this.squares.splice(idx, 1, this.index);
+        console.log(this.arrOfIdx)
+      }
+    } else {
+      console.log("hasta aqui nomás")
+    }
+    console.log(this.arrOfIdx)
+  };
+
+  startGame() {
+    if (this.arrOfIdx.length === 3) {
+      console.log("hey")
+      this.hidding.player1 = true;
+      this.hidddingAt = Array.from(this.arrOfIdx);
+      console.log("111",this.hidding,"original",this.arrOfIdx, "clone",this.hidddingAt)
+      this.newGame();
+    } 
+  };
+
 
   makeMove(idx: number) {
-    if (!this.squares[idx]){
-      this.squares.splice(idx, 1, this.player);
-      this.xIsNext = !this.xIsNext;
-    }
-    this.winner = this.calculateWinner();
-  };
-  calculateWinner() {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        this.squares[a] &&
-        this.squares[a] === this.squares[b] &&
-        this.squares[a] === this.squares[c]
-      ) {
-        return this.squares[a];
+    console.log(idx)
+    console.log(idx, !this.squares[idx], this.squares, this.index)
+    this.hidddingAt.forEach(a => {
+      if (!this.squares[idx]) {
+        if (idx === a) {
+          this.index = true;
+          this.squares.splice(idx, 1, this.index);
+          console.log("oii", this.squares)
+        }
+        else {
+          console.log('xau')
+          this.index = false;
+          this.squares.splice(idx, 1, this.index);
+        }
       }
-    }
-    return null;
+    })
   };
+
 };
